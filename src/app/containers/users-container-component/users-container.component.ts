74687810e0058a378d495b7d55cd90b74cc61ce1
AlphaWaves <dev@awaves.it>
2023-12-29 18:56:57 +0100

    User container manages User form and API Requests

--- src/app/containers/users-container-component/users-container.component.ts
+++ src/app/containers/users-container-component/users-container.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserData} from "../../interfaces/user";
 import {catchError, EMPTY, Observable, of, switchMap, tap} from "rxjs";
 import {UsersService} from "../../services/users.service";
 import {map} from "rxjs/operators";
+import {DialogsService} from "../../services/dialogs.service";
+import {GeneralDialogComponent} from "../../components/general-dialog/general-dialog.component";
+import {UserFormComponent} from "../../components/user-form/user-form.component";
 
 @Component({
   selector: 'users-container-component',
@@ -18,7 +21,8 @@ export class UsersContainerComponent implements OnInit {
   users$ = new Observable<UserData[]>()
   showModal = false;
 
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService,
+              private dialogService: DialogsService) {
     this.users$ =
       of(true)
         .pipe(
@@ -36,7 +40,6 @@ export class UsersContainerComponent implements OnInit {
             console.log(err)
             return EMPTY;
           })
-
         )
   }
 
@@ -47,6 +50,43 @@ export class UsersContainerComponent implements OnInit {
   }
 
   createUser() {
+
+    this.dialogService.open(GeneralDialogComponent, {
+      data: {
+        dialogTitle: 'Create user',
+        componentData: {},
+        component: UserFormComponent
+      }
+    })
+      .afterClosed()
+      .pipe(switchMap(value => value ? this.users$ : EMPTY))
+      .subscribe({
+        next: () => console.log()/*this.snackService.success('User created')*/,
+        error: (error) => console.log(error)/*this.snackService.error(error.message)*/,
+      })
+
+  }
+
+  editUser(userId: number) {
+
+    this.usersService.getUser(userId)
+      .subscribe(user => {
+
+        this.dialogService.open(GeneralDialogComponent, {
+          data: {
+            dialogTitle: 'Create user',
+            componentData: {entity: user},
+            component: UserFormComponent
+          }
+        })
+          .afterClosed()
+          .pipe(switchMap(value => value ? this.users$ : EMPTY))
+          .subscribe({
+            next: () => console.log()/*this.snackService.success('User created')*/,
+            error: (error) => console.log(error)/*this.snackService.error(error.message)*/,
+          })
+      })
+
 
   }
 }
